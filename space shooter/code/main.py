3d1c75ff59238d61eb1ca115b15631b16fdeaa5b
Tadijas1 <ctadeusz14@gmail.com>
2025-07-05 00:57:44 +0200

    Rectangles

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -13,9 +13,19 @@ running = True
 surf = pygame.Surface((100,100))
 surf.fill('darkorange')
 x = 100
+bounc = True
 
 #importing an image
 player_surf = pygame.image.load(join('space shooter', 'images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
+player_direction = 1
+
+meteor_surf = pygame.image.load(join('space shooter', 'images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
+
+laser_surf = pygame.image.load(join('space shooter', 'images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT-20))
+
 star_surf = pygame.image.load(join('space shooter', 'images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
@@ -30,9 +40,14 @@ while running:
 
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
 
-    x += 0.3
-    display_surface.blit(player_surf, (x, 150))
+    #player movment
+    player_rect.x += player_direction*0.2
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+    display_surface.blit(player_surf, player_rect)
     pygame.display.update()
 
 pygame.quit()
