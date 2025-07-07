061747033a40199550bfc801ff7f70f39a40851b
Tadijas1 <ctadeusz14@gmail.com>
2025-07-07 17:52:34 +0200

    sprites

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -2,7 +2,33 @@ import pygame
 from os.path import join
 from random import randint
 
-#general settings
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('space shooter', 'images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
+        self.direction = pygame.Vector2()
+        self.speed = 300
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction*self.speed*dt
+
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE]:
+            print('fire laser')
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups, surf):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = (randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)))
+
+# general settings
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -15,58 +41,33 @@ surf = pygame.Surface((100,100))
 surf.fill('darkorange')
 x = 100
 
-#importing an image
-player_surf = pygame.image.load(join('space shooter', 'images', 'player.png')).convert_alpha()
-player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
-player_direction = pygame.math.Vector2()
-player_speed = 300
+all_sprites = pygame.sprite.Group()
+star_surf = pygame.image.load(join('space shooter', 'images', 'star.png')).convert_alpha()
+for i in range(20):
+    Star(all_sprites, star_surf)
+player = Player(all_sprites)
 
+# importing an image
 meteor_surf = pygame.image.load(join('space shooter', 'images', 'meteor.png')).convert_alpha()
 meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
 
 laser_surf = pygame.image.load(join('space shooter', 'images', 'laser.png')).convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT-20))
 
-star_surf = pygame.image.load(join('space shooter', 'images', 'star.png')).convert_alpha()
-star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
-
-
 while running:
     dt = clock.tick()/1000
-    #print(clock.get_fps())
-
-    #event loop
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        #if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
-        #    print(1)
-        #if event.type == pygame.MOUSEMOTION:
-        #    player_rect.center = event.pos
+            running = False 
 
-    #input
-    #print(pygame.mouse.get_pos())  print(pygame.mouse.get_rel())
-    keys = pygame.key.get_pressed()
-    # player input
-    player_direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-    player_direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+    # update
+    all_sprites.update(dt)
 
-    player_direction = player_direction.normalize() if player_direction else player_direction
-    player_rect.center += player_direction*player_speed*dt
-
-    #laser input
-    just_keys = pygame.key.get_just_pressed()
-    if just_keys[pygame.K_SPACE]:
-        print('fire laser')
-
-    #draw in game
+    # draw in game
     display_surface.fill('black')
+    all_sprites.draw(display_surface)
 
-    for pos in star_positions:
-        display_surface.blit(star_surf, pos)
-    display_surface.blit(meteor_surf, meteor_rect)
-    display_surface.blit(laser_surf, laser_rect)
-    display_surface.blit(player_surf, player_rect)
     pygame.display.update()
 
 pygame.quit()
